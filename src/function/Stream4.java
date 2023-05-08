530afc817eb01b55dd7ff0ba8d1864360b52a7b9
yushin <kkad45@naver.com>
2023-05-09 06:04:44 +0900

    feat 이전 내용 업로드

---
+++ src/function/Stream4.java
@@ -0,0 +1,16 @@
+package function;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class Stream4 {
+    public static void main(String[] args) {
+        LocalDate nowMinusTwoMonth = LocalDate.now().minusMonths(2);
+        Stream.iterate(nowMinusTwoMonth, day -> day.plusDays(1))
+                .limit(ChronoUnit.DAYS.between(nowMinusTwoMonth, LocalDate.now()) + 1)
+                .forEach(item -> System.out.println("item > " + item));
+    }
+}
