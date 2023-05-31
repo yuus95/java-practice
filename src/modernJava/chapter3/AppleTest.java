fad6a23ae3a96c633eb6045e8f11f199d155a56f
yushin <kkad45@naver.com>
2023-06-01 08:38:31 +0900

    feat 모던 자바 인 액션 4장 일부정리(1)
    - 컬렉션과 SQL 질의의 차이점
    - 스트림의 특징

--- src/modernJava/chapter3/AppleTest.java
+++ src/modernJava/chapter3/AppleTest.java
@@ -10,17 +10,34 @@ import java.util.List;
 
 public class AppleTest {
     public static void main(String[] args) {
-        Apple[] arrays  = {new Apple("test", 100), new Apple("test123", 100), new Apple("test2", 200)};
+        Apple[] arrays = {new Apple("test", 100), new Apple("test123", 100), new Apple("test2", 200)};
         List<Apple> apples = new ArrayList<>(Arrays.asList(arrays));
 
-        apples.sort(Comparator.comparing(Apple::getWeight).thenComparing((a1) -> -a1.getName().length()));
-//        apples.sort(Comparator.comparing(Apple::getWeight));
+        // sort 메서드는 함수형 인터페이스를 사용하므로 동작을 코드로 전달할 수 있다.
+        apples.sort(new AppleComparator());
 
-        apples.forEach(item -> {
-            System.out.println("item" + item);
+        // 익명클래스
+        apples.sort(new Comparator<Apple>() {
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return o1.getWeight().compareTo(o2.getWeight());
+            }
         });
 
+        //람다표현식
+        apples.sort((o1, o2) -> o1.getWeight().compareTo(o2.getWeight()));
 
+        // 메소드참조
+        apples.sort(Comparator.comparing(Apple::getWeight));
+
+    }
+
+    static class AppleComparator implements Comparator<Apple>{
+        @Override
+        public int compare(Apple o1, Apple o2) {
+
+            return o1.getWeight().compareTo(o2.getWeight());
+        }
     }
 
 
