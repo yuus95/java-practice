cb65a9772bf6375961914a1a2a4f3eec4f3efc7d
yushin <kkad45@naver.com>
2023-07-10 21:52:26 +0900

    feat 모던 자바인 액션 5장 일부 정리(2) map, flatmap
    - map 내용 정리 및 flatmap 사용방법 정리

--- src/modernJava/chapter5/Main.java
+++ src/modernJava/chapter5/Main.java
@@ -1,7 +1,11 @@
 package modernJava.chapter5;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +20,29 @@ public class Main {
                 new Dish("salmon", false, 450, Dish.Type.FISH)
         );
 
+        List<Integer> firstList = List.of(1, 2, 3);
+        List<Integer> secondList = List.of(3, 4);
+        List<int[]> collect = firstList.stream()
+                .flatMap(i -> secondList.stream()
+                        .filter(j -> i == j )
+                        .map(j -> new int[]{i, j}))
+                .collect(Collectors.toList());
 
+        List<Integer> temp = List.of(List.of(1), List.of(2))
+                .stream()
+                .flatMap(i -> secondList.stream())
+                .collect(Collectors.toList());
+        Stream<Dish> stream = menu.stream();
+        List<String> stringTemp = List.of("temp");
+        Stream<Integer> integerStream = List.of(1, 2, 3, 4).stream();
+        Stream<String> stringStream = integerStream.map(i -> String.valueOf(i));
+        Stream<String> stringStream1 = stringStream.flatMap(i -> stringTemp.stream()
+                .map(test -> i + test));
+    }
+
+    public static void print(List<Object> dishes) {
+        for (Object dish : dishes) {
+            System.out.println(dish);
+        }
     }
 }
